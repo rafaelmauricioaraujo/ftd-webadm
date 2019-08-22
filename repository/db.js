80654242af503c60093c1ea40b99717fbe0d418d
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2019-08-22 17:48:50 -0300

    adicionando dotenv e configurando no db.js e index.js

--- repository/db.js
+++ repository/db.js
@@ -1,12 +1,12 @@
-var mongoClient = require('mongodb').MongoClient;
-var uri = "mongodb://queryUser:ftdigital1234@ds151994.mlab.com:51994/prefeitura";
 
-mongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(conn => global.conn = conn.db('prefeitura'))
+var mongoClient = require('mongodb').MongoClient;
+
+mongoClient.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(conn => global.conn = conn.db(process.env.DB))
 .catch(err => console.log(err));
 
 function findAll(callback){
-    global.conn.collection('servicos_web_admin').find({}).toArray(callback);
+    global.conn.collection(process.env.COLL).find({}).toArray(callback);
 }
 
 module.exports = { findAll }
