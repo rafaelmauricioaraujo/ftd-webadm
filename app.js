d6412df4ef0264921ebe2f31fd1d52dcea437c43
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2019-08-26 14:53:39 -0300

    cnfigurando parametros de sessao

--- app.js
+++ app.js
@@ -29,6 +29,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+/** Configurando o app para usar os módulos de autenticacao e gerenciamento */
+require('./auth')(passport);
+app.use(session({
+  store: new MongoStore({
+    db: global.db,
+    ttl: 30 * 60 /** 30 minutos de sessão */
+  }),
+  secret: 'ftdigitalsecret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
